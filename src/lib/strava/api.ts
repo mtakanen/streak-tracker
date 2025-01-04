f47dee7a5c6c4ca7516b9a4f49b6336d8a3f7bf4
Marko Takanen <marko.takanen@sok.fi>
2025-01-04 17:29:53 +0200

    add result pagination

--- src/lib/strava/api.ts
+++ src/lib/strava/api.ts
@@ -1,23 +1,43 @@
 import { StravaActivity } from '@/types/strava';
 
 const ATHLETE_ACTIVITIES_URL = 'https://www.strava.com/api/v3/athlete/activities';
+
 export async function getStravaActivities(after: number): Promise<StravaActivity[]> {
   const token = localStorage.getItem('stravaAccessToken');
   if (!token) {
     throw new Error('No access token found');
   }
 
-  const response = await fetch(`${ATHLETE_ACTIVITIES_URL}?after=${after}`, {
-    headers: {
-      'Authorization': `Bearer ${token}`,
-    },
-  });
+  let allActivities: StravaActivity[] = [];
+  let page = 1;
+  let perPage = 30;
+  let hasMoreActivities = true;
 
-  if (!response.ok) {
-    console.log(response);
-    throw new Error('Failed to fetch activities');
+  while (hasMoreActivities) {
+    const response = await fetch(`${ATHLETE_ACTIVITIES_URL}?after=${after}&per_page=${perPage}&page=${page}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      console.log(response);
+      if (response.status === 429) {
+        throw new Error('Too many requests to Strava API');
+      } else {
+        throw new Error('Failed to fetch activities');
+      }
+    }
+
+    const data: StravaActivity[] = await response.json();
+    allActivities = allActivities.concat(data);
+
+    if (data.length < perPage) {
+      hasMoreActivities = false;
+    } else {
+      page++;
+    }
   }
 
-  const data = await response.json();
-  return data;
+  return allActivities;
 }
