5e2d7da9b7796ef8d226a3219d2d2028ba5eab3a
Marko Takanen <mtakanen@gmail.com>
2025-01-21 21:20:00 +0200

    update support

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { INITIAL_LOAD_MONTHS } from '@/lib/strava/config';
 
 const SupportPage = () => {
   return (
     <Card className="w-full max-w-md mx-auto mt-10">
       <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl font-bold">Normi Run Support</CardTitle>
+        <CardTitle className="text-2xl font-bold">normi.run Support</CardTitle>
       </CardHeader>
       <CardContent className="space-y-4">
         <div>
@@ -17,7 +18,7 @@ const SupportPage = () => {
           <ul className="list-disc list-inside mt-2 text-gray-700">
             <li>Log in to your Strava account to start tracking your run streak</li>
             <li>Check your streaks and progress on the dashboard</li>
-            <li>Streaks are calculated based on the daily 25 min goal 
+            <li>Streaks are calculated based on the daily 25 min goal. 
                 Ensure the sum of moving time meet the daily goal to maintain your streak</li>
             <li>If you encounter any issues, log out from the menu and log in again</li>
           </ul>
@@ -28,22 +29,27 @@ const SupportPage = () => {
             Please be aware of the following limitations:
           </p>
           <ul className="list-disc list-inside mt-2 text-gray-700">
-            <li>Normi Run currently supports only running activities</li>
-            <li>Data synchronization with Strava may take a few minutes</li>
+            <li>normi.run currently supports only running activities</li>
+            <li>Data synchronization with Strava may take a few minutes. Reload the page if needed</li>
+            <li>Maximum streak is limited to {INITIAL_LOAD_MONTHS*30} days</li>
             <li>If you encounter any persistent issues, please contact the developer for assistance</li>
           </ul>
         </div>
         <div>
         <h2 className="text-xl font-semibold">Privacy</h2>
             <p className="mt-2 text-gray-700">
-            Your privacy is important to us. Here are some details on how we handle your data:
+            Your privacy is important to us.<br />
+            TL;DR: <span className='font-bold'>we cannot access any of your Strava data!</span> 
             </p>
             <ul className="list-disc list-inside mt-2 text-gray-700"></ul>
-            <li>To find your initial streak Normi Run reads your Strava activities up to one year</li>
-            <li>Normi Run caches up to one week of your Strava data in your browser local storage</li>
-            <li>Normi Run does not write anything to your Strava account</li>
-            <li>Normi Run does not share your data with third parties</li>
-            <li>Normi Run does not use cookies or tracking scripts</li>
+            <li>All athlete and activity data is stored in your browser&apos;s local storage only. 
+              You can delete them anytime by using Log out from the Menu</li>
+            <li>normi.run caches up to one week of activity data</li>
+            <li>normi.run does not share your data with third parties</li>
+            <li>normi.run does not use cookies or tracking scripts</li>
+            <li>normi.run can rename activity back to Strava if the following permission is given upon Strava login:</li>
+            <span className='italic'>Upload your activities from normi.run to Strava</span>
+            <li>You can revoke access at: <a href="https://www.strava.com/settings/apps" target="_blank">https://www.strava.com/settings/apps</a></li>
         </div>
         <div className="text-center mt-4">
           <Link href="/" className="text-black-600 hover:underline">
