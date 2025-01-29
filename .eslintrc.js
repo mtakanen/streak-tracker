726413882efa44bc02b986d113d8056643f1c3bc
Marko Takanen <mtakanen@gmail.com>
2025-01-29 17:23:50 +0200

    add formatter

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', // Make sure this is always the last configuration in the extends array.
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    // Add any additional rules or overrides here
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
