ce7efec6038f8f8312bfa9cdbb2c63d0590cfaed
Marko Takanen <mtakanen@gmail.com>
2025-02-03 22:30:32 +0200

    simplify athlete data

--- src/pages/auth/callback/index.tsx
+++ src/pages/auth/callback/index.tsx
@@ -32,28 +32,35 @@ export default function AuthCallback() {
         setLoading(true);
         const response = await axios.post<StravaTokenData>(STRAVA_CALLBACK_PAGE, { code, scope })
         const data = response.data;
+        const simplifiedAthlete = {
+          id: data.athlete.id,
+          firstname: data.athlete.firstname,
+          lastname: data.athlete.lastname,
+          username: data.athlete.username,
+          profile_medium: data.athlete.profile_medium,
+        };
         // Store tokens
         localStorage.setItem('stravaAccessToken', data.access_token);
         localStorage.setItem('stravaRefreshToken', data.refresh_token);
         localStorage.setItem('stravaTokenExpiry', (Date.now() + (data.expires_in * 1000)).toString());
-        localStorage.setItem('stravaAthlete', JSON.stringify(data.athlete));
+        localStorage.setItem('stravaAthlete', JSON.stringify(simplifiedAthlete));
         // Set the scope in the global state
         setScope(scope);
         localStorage.setItem('scope', scope || '');
       } catch (error) {
         if (axios.isAxiosError(error)) {
           if (error.response && error.response.data) {
-            console.error('Error during token exchange:', error.response.data);
+            // console.error('Error during token exchange:', error.response.data);
             route = `/?error=${encodeURIComponent(error.response.data.message)}`;      
           } else if (error.request) {
-            console.error('Network error:', error.message);
+            // console.error('Network error:', error.message);
             route = `/?error=${encodeURIComponent('Network error: Failed to exchange code')}`;
           } else {
-            console.error('Error message:', error.message);
+            // console.error('Error message:', error.message);
             route = `/?error=${encodeURIComponent(error.message)}`;  
           }
         } else {
-          console.error('Unknown Error:', error);
+          // console.error('Unknown Error:', error);
           route = `/?error=${encodeURIComponent('Unknown error: Failed to exchange code')}`;
         }
       } finally {
