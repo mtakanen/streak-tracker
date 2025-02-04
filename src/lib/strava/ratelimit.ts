5ebc4e64f5b720948bbc0ca5bfdefa7105d74936
Marko Takanen <mtakanen@gmail.com>
2025-02-04 19:30:52 +0200

    add daily limiter

--- src/lib/strava/ratelimit.ts
+++ src/lib/strava/ratelimit.ts
@@ -3,26 +3,36 @@ import { Ratelimit } from "@upstash/ratelimit";
 import crypto from "crypto";
 import { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN, STRAVA_API_LIMIT } from './config';
 
-const ratelimit = new Ratelimit({
-    redis: new Redis(
-      {
+const redis = new Redis(
+    {
         url: UPSTASH_REDIS_REST_URL, 
         token: UPSTASH_REDIS_REST_TOKEN
-      }
-    ),
-    limiter: Ratelimit.slidingWindow(STRAVA_API_LIMIT, "15 m"),
-    prefix: 'normirun-request',
-    analytics: false,
-  });
+    }
+);
+const ratelimit = {
+    quarterlyLimit: new Ratelimit({
+          redis,
+          prefix: "normirun-15m",
+          analytics: false,
+          limiter: Ratelimit.slidingWindow(STRAVA_API_LIMIT, "15 m"),
+        }),
+    dailyLimit: new Ratelimit({
+          redis,
+          prefix: "normirun-24h",
+          analytics: false,
+          limiter: Ratelimit.slidingWindow(STRAVA_API_LIMIT * 3, "24h"),
+        }),
+};
   
-  export default async function accountCanMakeRequest(
+export default async function accountCanMakeRequest(
     accountId: string | number
   ) {
     const encryptedAccountId = crypto
       .createHash("sha256")
       .update(accountId.toString())
       .digest("hex");
-    const { success } = await ratelimit.limit(encryptedAccountId);
-    return success;
+    const quarterly  = await ratelimit.quarterlyLimit.limit(encryptedAccountId);
+    const daily  = await ratelimit.dailyLimit.limit(encryptedAccountId);
+    return { quarterly: quarterly.success, daily: daily.success };
   }
   
